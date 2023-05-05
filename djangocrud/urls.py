ebe7ee22d047cb19813331722137a66998ab8161
julianParrax <julianfranco.parra@gmail.com>
2023-05-04 23:59:35 -0300

    Added: 3. Creando formulario Signup

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from tasks import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.helloworld)
+    path('', views.helloworld),
+    path('signup/', views.helloworld)
 ]
