61030f137bff87b9d10dc237c7e1a30f7b14b365
julianParrax <julianfranco.parra@gmail.com>
2023-05-07 03:02:12 -0300

    Added:5.manejando errores

--- tasks/views.py
+++ tasks/views.py
@@ -21,7 +21,13 @@ def signup(request):
                 user.save()
                 return HttpResponse("User created successfully") 
             except:
-                return HttpResponse("username already exists")
-        return HttpResponse("Password do not match")
+                return render(request, 'signup.html',{
+                "form": UserCreationForm(),
+                "error": "Username already exists"
+            })
+        return render(request, 'signup.html',{
+                "form": UserCreationForm(),
+                "error": "Password do not match"
+            })
         
         
