faec3509a5a90b87d78af6b4bd4dda6416828089
julianParrax <julianfranco.parra@gmail.com>
2023-05-11 20:01:35 -0300

    Added: 10. Added Task Model(modelo de tareas) con ORM para generar la tabla y columnas en la db para guardar tareas y los usuarios que las hicieron. Ya se comienza a trabajar con el /admin/ de django

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+#aca voy a hacer que mi tabla SQL generada tenga acceso al panel de administrador, para qu asi pueda crear mis tareas y se guarden en la tabla. para lo cual traigo mi modelo llamado  'Tasks'
+from .models import Task
+
+#esta clsae es para mostrar por pantalla campos que son de solo lectura. En este caso el atributo 'created': que muestra las tareas que se han creado y que no puede ser modificado por que ese campo es de solo lectura
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ("created",)
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+#Esto va  acrear una tabla de sql, que se va a llamar tarea
+#con esta clase django va  a crear la tabla; tambien le indico que tipo de dato quiero que sea; las columnas son los atributos de la clase
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #esto es para que muestre el nombre de la tarea y no solo un objeto;
+    #  self es como una referencia a la clase del modelo
+    def __str__(self):
+        return self.title + ' -by ' + self.user.username
+
+
